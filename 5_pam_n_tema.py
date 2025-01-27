8c26f647a7d2bc20ecfe64bb84cbc7a3c26520ca
Saulius <sauliusenator@gmail.com>
2025-01-27 19:37:23 +0200

    Commit message

---
+++ 5_pam_n_tema.py
@@ -0,0 +1,21 @@
+user_name = input('Please write your name: ')
+
+#  print(f' Hello, {user_name}!')
+
+# text = 'ABCcda123'
+# sarasas_list = ['1', '2', '3']
+# res = len(text)  #  length
+# print(res)
+# print(len(sarasas_list))
+#
+# number_of_elements = 0
+# for _ in sarasas_list:
+#     number_of_elements += 1
+#     print(number_of_elements)
+
+# sarasas_skaiciu = [12, 15, 18, 20, 60, 1]
+# print(sum(sarasas_skaiciu))
+# res = 0
+# for i in sarasas_skaiciu:
+#     res += 1
+#     print(res)
