13691ac2cf23a6352c0b8a3f7e7c86010674d1de
Saulius <sauliusenator@gmail.com>
2025-02-03 18:42:46 +0200

    Commit message

---
+++ 13p.Task_1.py
@@ -0,0 +1,12 @@
+
+import datetime
+res = datetime.datetime.today()
+
+metai = res.year
+menesis = res.month
+diena = res.day
+valanda = res.hour
+minute = res.minute
+sekunde = res.second
+
+print(f"Dabar yra {valanda}:{minute}, {diena}-{menesis}-{metai}")
