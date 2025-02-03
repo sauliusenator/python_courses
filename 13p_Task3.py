45e3304c29a2c5c10b4a847ef7a3a9a0b1b264a3
Saulius <sauliusenator@gmail.com>
2025-02-03 20:46:04 +0200

    Commit message

---
+++ 13p_Task3.py
@@ -0,0 +1,15 @@
+import datetime
+#5. ###################################
+ivestis = '2020-01-01'
+my_datetime = datetime.datetime.strptime(ivestis, '%Y-%m-%d')
+print(my_datetime)
+#6. ###################################
+
+
+# Įvedame datą ir laiką
+ivedam = '2022-12-31, 23:59:59'
+
+my_datetime = datetime.datetime.strptime(ivedam, '%Y-%m-%d, %H:%M:%S')
+formatted_time = my_datetime.strftime('%d/%m/%Y, %H:%M:%S')
+print(formatted_time)
+
